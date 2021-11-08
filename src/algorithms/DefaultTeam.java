b24e761bed467465ee3fe7195e54c8ec2d27ee06
CTeillet <teillet.corentin@neuf.fr>
2021-11-08 23:45:26 +0100

    modif corps kmeans

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -5,22 +5,29 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class DefaultTeam {
-    public static final int k = 5;
+    public static final int K = 5;
     public ArrayList<ArrayList<Point>> calculKMeans(ArrayList<Point> points) {
-        //1. Choisir K points aleatoires parmi points existants et le copier
-        //2. Parcourir S et associer au points significatif le plus proche
-        //3. Faire le barycentre pour les points significatif
-        //4. Revenir étape 2
         ArrayList<ArrayList<Point>> res = new ArrayList<>();
         ArrayList<Point> means = new ArrayList<>();
-        Random r = new Random();
-        for (int i=0; i<k; i++){
-            int rdP = r.nextInt(points.size());
-            ArrayList<Point> temp = new ArrayList<>();
-            temp.add(points.get(rdP));
-            res.add(temp);
-            means.add(points.get(rdP));
+
+        initialization(points, means, res);
+        firstAquisition(points, res, means);
+
+        //Variables initialization for the loop
+        boolean change = true;
+        int nbIter = 0;
+
+        //Re do Acquisition and Analyse until the groups do not change anymore
+        while (change){
+            nbIter++;
+            analyse(res, means);
+            change = acquisition(res, means);
         }
+        System.out.println("Nb Iter : " + nbIter);
+        return res;
+    }
+
+    private void firstAquisition(ArrayList<Point> points, ArrayList<ArrayList<Point>> res, ArrayList<Point> means) {
         for(Point p : points){
             if (means.contains(p)) continue;
             int min = -1;
@@ -31,35 +38,46 @@ public class DefaultTeam {
             }
             res.get(min).add(p);
         }
-        while (true){
-            for (int i=0; i<k; i++){
-                Point bary = barycentre(res.get(i));
-                means.set(i, bary);
-            }
-            boolean change = false;
-            for (int i = 0; i< res.size(); i++){
-                for (int j = 0; j< res.get(i).size(); j++){
-                    int min = -1;
-                    for (int k = 0; k< means.size(); k++){
-                        //System.out.println("I : "+i+" J : "+j+" K : "+k);
-                        if (res.get(i).get(j).distance(means.get(k)) < res.get(i).get(j).distance(means.get(i))){
-                            min = k;
-                        }
-                    }
-                    if(min!=-1 && min!=i){
-                        change = true;
-                        Point p = res.get(i).remove(j);
-                        j--;
-                        res.get(min).add(p);
+    }
 
+    public void initialization(ArrayList<Point> points, ArrayList<Point> means, ArrayList<ArrayList<Point>> clusters) {
+        Random r = new Random();
+        for (int i = 0; i < K; i++) {
+            int rdP = r.nextInt(points.size());
+            means.add(points.get(rdP));
+            clusters.add(new ArrayList<>());
+            clusters.get(i).add(points.get(rdP));
+            points.remove(rdP);
+        }
+    }
+
+    public boolean acquisition(ArrayList<ArrayList<Point>> clusters, ArrayList<Point> means) {
+        boolean change = false;
+        for (int i = 0; i< clusters.size(); i++){
+            for (int j = 0; j< clusters.get(i).size(); j++){
+                int min = -1;
+                for (int k = 0; k< means.size(); k++){
+                    if (clusters.get(i).get(j).distance(means.get(k)) < clusters.get(i).get(j).distance(means.get(i))){
+                        min = k;
                     }
                 }
+                if(min!=-1 && min!=i){
+                    change = true;
+                    Point p = clusters.get(i).remove(j);
+                    j--;
+                    clusters.get(min).add(p);
+
+                }
             }
-            if (!change) break;
         }
+        return change;
+    }
 
-
-        return res;
+    public void analyse(ArrayList<ArrayList<Point>> clusters, ArrayList<Point> means) {
+        for (int i = 0; i < clusters.size(); i++) {
+            Point bary = barycentre(clusters.get(i));
+            means.set(i, bary);
+        }
     }
 
     private Point barycentre(ArrayList<Point> points) {
@@ -73,8 +91,8 @@ public class DefaultTeam {
     }
 
     public ArrayList<ArrayList<Point>> calculKMeansBudget(ArrayList<Point> points) {
-        ArrayList<Point> rouge = new ArrayList<Point>();
-        ArrayList<Point> verte = new ArrayList<Point>();
+        ArrayList<Point> rouge = new ArrayList<>();
+        ArrayList<Point> verte = new ArrayList<>();
 
         for (int i = 0; i < points.size() / 2; i++) {
             rouge.add(points.get(i));
@@ -82,13 +100,10 @@ public class DefaultTeam {
         }
         if (points.size() % 2 == 1) rouge.add(points.get(points.size() / 2));
 
-        ArrayList<ArrayList<Point>> kmeans = new ArrayList<ArrayList<Point>>();
+        ArrayList<ArrayList<Point>> kmeans = new ArrayList<>();
         kmeans.add(rouge);
         kmeans.add(verte);
 
-        /*******************
-         * PARTIE A ECRIRE *
-         *******************/
         return kmeans;
     }
 }
